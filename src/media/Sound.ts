5d0f62e9aa7acd28a2d135cdc6f3f7079e7ac1a7
fly <fly@load-page.com>
2017-12-01 19:57:23 +0800

    sound return channel

--- src/media/Sound.ts
+++ src/media/Sound.ts
@@ -1,9 +1,10 @@
 namespace layer.media {
 
 	export class Sound {
-		public static play(resName: string, loops: number = 1) {
+		public static play(resName: string, loops: number = 1) : egret.SoundChannel | null
+		{
 			let sound: egret.Sound = RES.getRes(resName);
-			if (sound) sound.play(0, loops);
+			return sound ? sound.play(0, loops) : null;
 		}
 	}
 }
